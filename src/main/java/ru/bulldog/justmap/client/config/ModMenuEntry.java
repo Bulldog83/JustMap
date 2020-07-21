94518a8050be1e356574d2a40396dbde6fca28a2
Aleksey <bulldog.seregin@gmail.com>
2020-07-21 08:47:56 +0300

    Updated Fabric, Cloth Config and ModMenu

--- src/main/java/ru/bulldog/justmap/client/config/ModMenuEntry.java
+++ src/main/java/ru/bulldog/justmap/client/config/ModMenuEntry.java
@@ -1,24 +1,15 @@
 package ru.bulldog.justmap.client.config;
 
-import ru.bulldog.justmap.JustMap;
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 
-import net.minecraft.client.gui.screen.Screen;
 import net.fabricmc.api.Environment;
 import net.fabricmc.api.EnvType;
 
-import java.util.function.Function;
-
 @Environment(EnvType.CLIENT)
 public class ModMenuEntry implements ModMenuApi {
-	
 	@Override
-	public String getModId() {
-		return JustMap.MODID;
-	}
-	
-	@Override
-	public Function<Screen, ? extends Screen> getConfigScreenFactory() {
-		return (parent) -> ConfigFactory.getConfigScreen(parent);
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
+		return ConfigFactory::getConfigScreen;
 	}
 }
