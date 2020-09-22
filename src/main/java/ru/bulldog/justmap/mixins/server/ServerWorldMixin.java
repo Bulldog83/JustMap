a60d15eb9c4bb343473585b64118912e2be60f97
Aleksey <bulldog.seregin@gmail.com>
2020-09-22 14:25:56 +0300

    Update ServerWorldMixin.java

--- src/main/java/ru/bulldog/justmap/mixins/server/ServerWorldMixin.java
+++ src/main/java/ru/bulldog/justmap/mixins/server/ServerWorldMixin.java
@@ -36,30 +36,30 @@ public abstract class ServerWorldMixin extends World {
 
 	@Inject(method = "onBlockChanged", at = @At("HEAD"))
 	public void onBlockChanged(BlockPos pos, BlockState oldBlock, BlockState newBlock, CallbackInfo info) {
-		if (!Objects.equals(oldBlock.getBlock(), newBlock.getBlock())) {
-			if (!StateUtil.isAir(newBlock)) {
-				Identifier blockId = Registry.BLOCK.getId(newBlock.getBlock());
-				String modId = blockId.getNamespace();
-				String id = blockId.getPath();
-				String stateFile = String.format("/assets/%s/blockstates/%s.json", modId, id);
-				try {
-					JsonObject stateJson = JsonFactory.getJsonObject(stateFile);
-					if (stateJson.has("variants")) {
-						JsonObject variants = stateJson.getAsJsonObject("variants");
-						variants.entrySet().forEach((entry) -> {
-							System.out.println(entry.getKey());
-							if (entry.getValue().isJsonArray()) {
-								JsonArray entryArray = entry.getValue().getAsJsonArray();
-								entryArray.forEach(elem -> {
-								});
-							} else {
-							}
-						});
-					}
-				} catch (Exception ex) {
-					System.out.println(String.format("Blockstate %s not available.", stateFile));
-				}
-			}
-		}
+//		if (!Objects.equals(oldBlock.getBlock(), newBlock.getBlock())) {
+//			if (!StateUtil.isAir(newBlock)) {
+//				Identifier blockId = Registry.BLOCK.getId(newBlock.getBlock());
+//				String modId = blockId.getNamespace();
+//				String id = blockId.getPath();
+//				String stateFile = String.format("/assets/%s/blockstates/%s.json", modId, id);
+//				try {
+//					JsonObject stateJson = JsonFactory.getJsonObject(stateFile);
+//					if (stateJson.has("variants")) {
+//						JsonObject variants = stateJson.getAsJsonObject("variants");
+//						variants.entrySet().forEach((entry) -> {
+//							System.out.println(entry.getKey());
+//							if (entry.getValue().isJsonArray()) {
+//								JsonArray entryArray = entry.getValue().getAsJsonArray();
+//								entryArray.forEach(elem -> {
+//								});
+//							} else {
+//							}
+//						});
+//					}
+//				} catch (Exception ex) {
+//					System.out.println(String.format("Blockstate %s not available.", stateFile));
+//				}
+//			}
+//		}
 	}
 }
