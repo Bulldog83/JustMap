ecf24110d0e8dabb7de7e7ac63d250b153e210f2
Magnus Ihse Bursie <mag@icus.se>
2021-11-14 00:58:43 +0100

    Remove old junk

--- src/main/java/ru/bulldog/justmap/map/data/fast/MapChunk.java
+++ src/main/java/ru/bulldog/justmap/map/data/fast/MapChunk.java
@@ -7,8 +7,6 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.MapColor;
 import net.minecraft.tag.FluidTags;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.Heightmap;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.WorldChunk;
@@ -25,34 +23,21 @@ public class MapChunk {
 	private final Layer layer;
 	private final int level;
 
+	// Note that color data has a different layout than the scouted data
 	private final byte[][] colorData = new byte[MapRegionLayer.CHUNK_SIZE][MapRegionLayer.CHUNK_SIZE * MapRegionLayer.BYTES_PER_PIXEL];
-	private final byte[][] heightData = new byte[MapRegionLayer.CHUNK_SIZE][MapRegionLayer.CHUNK_SIZE];
-	private final int[][] blockStateData = new int[MapRegionLayer.CHUNK_SIZE][MapRegionLayer.CHUNK_SIZE];
 
 	private final byte[][] solidY = new byte[MapRegionLayer.CHUNK_SIZE][MapRegionLayer.CHUNK_SIZE];
 	private final byte[][] transparentY = new byte[MapRegionLayer.CHUNK_SIZE][MapRegionLayer.CHUNK_SIZE];
 	private final byte[][] waterY = new byte[MapRegionLayer.CHUNK_SIZE][MapRegionLayer.CHUNK_SIZE];
 
-	private final BlockState[][] solidBlock = new BlockState[MapRegionLayer.CHUNK_SIZE][MapRegionLayer.CHUNK_SIZE];
-	private final BlockState[][] transparentBlock = new BlockState[MapRegionLayer.CHUNK_SIZE][MapRegionLayer.CHUNK_SIZE];
+	private final int[][] solidBlock = new int[MapRegionLayer.CHUNK_SIZE][MapRegionLayer.CHUNK_SIZE];
+	private final int[][] transparentBlock = new int[MapRegionLayer.CHUNK_SIZE][MapRegionLayer.CHUNK_SIZE];
 
 	public MapChunk(int relRegX, int relRegZ, Layer layer, int level) {
 		this.relRegX = relRegX;
 		this.relRegZ = relRegZ;
 		this.layer = layer;
 		this.level = level;
-	}
-
-	private int getChunkRelative(int coord) {
-		return coord & 15;
-	}
-
-	private int getChunkRelativeX(BlockPos blockPos) {
-		return getChunkRelative(blockPos.getX());
-	}
-
-	private int getChunkRelativeZ(BlockPos blockPos) {
-		return getChunkRelative(blockPos.getZ());
 	}
 
 	private void examinePos(World world, WorldChunk worldChunk, BlockPos.Mutable thisBlockPos) {
@@ -99,8 +84,8 @@ public class MapChunk {
 		transparentY[xOffset][zOffset] = (byte) transparent_y;
 		waterY[xOffset][zOffset] = (byte) water_y;
 
-		solidBlock[xOffset][zOffset] = solid_block;
-		transparentBlock[xOffset][zOffset] = transparent_block;
+		solidBlock[xOffset][zOffset] = Block.getRawIdFromState(solid_block);
+		transparentBlock[xOffset][zOffset] = Block.getRawIdFromState(transparent_block);
 	}
 
 	private int blockColorChunkFromCache(World world, WorldChunk worldChunk, BlockPos pos) {
@@ -110,8 +95,8 @@ public class MapChunk {
 		int transparent_y = transparentY[xOffset][zOffset];
 		int water_y = waterY[xOffset][zOffset];
 
-		BlockState solid_block = solidBlock[xOffset][zOffset];
-		BlockState transparent_block = transparentBlock[xOffset][zOffset];
+		BlockState solid_block = Block.getStateFromRawId(solidBlock[xOffset][zOffset]);
+		BlockState transparent_block =  Block.getStateFromRawId(transparentBlock[xOffset][zOffset]);
 
 		int shade = 0;
 
@@ -219,13 +204,18 @@ public class MapChunk {
 	}
 
 	public void updateBlock(BlockPos blockPos, BlockState blockState) {
-		int x = getChunkRelativeX(blockPos);
-		int z = getChunkRelativeZ(blockPos);
-
-	//    int color = Colors.INSTANCE.getBlockColor(blockState);
-		int color = blockColorPos(FastMapManager.MANAGER.getFastWorldMapper().getWorld(), blockPos, blockState);
-
-		setColor(x, z, color);
+		int x = blockPos.getX() & 15;
+		int z = blockPos.getZ() & 15;
+		int relevantY = solidY[x][z];
+		if (blockPos.getY() >= relevantY) {
+			BlockPos.Mutable pos = new BlockPos.Mutable();
+			pos.set(blockPos);
+			World world = FastMapManager.MANAGER.getFastWorldMapper().getWorld();
+			WorldChunk worldChunk = world.getWorldChunk(pos);
+			examinePos(world, worldChunk, pos);
+			int color = blockColorChunkFromCache(world, worldChunk, blockPos);
+			setColor(x, z, color);
+		}
 	}
 
 	public void writeToTextureBuffer(ByteBuffer buffer) {
@@ -244,113 +234,5 @@ public class MapChunk {
 		colorData[z][xOffset + 1] = (byte) (color & 255);
 		colorData[z][xOffset + 2] = (byte) ((color >> 8) & 255);
 		colorData[z][xOffset + 3] = (byte) ((color >> 16) & 255);
-	}
-
-	private void setHeight(int x, int z, int height) {
-		heightData[z][x] = (byte) height;
-	}
-
-	private int getHeight(int x, int z) {
-		if (x < 0 || z < 0) return 0;
-
-		return heightData[z][x];
-	}
-
-	public BlockState getBlockState(int x, int z) {
-		return Block.getStateFromRawId(blockStateData[z][x]);
-	}
-
-	public void setBlockState(int x, int z, BlockState blockState) {
-		blockStateData[z][x] = Block.getRawIdFromState(blockState);
-	}
-
-
-	private int blockColorChunk(WorldChunk worldChunk, BlockPos pos) {
-		// return ColorUtil.blockColor(world, blockState, pos);
-		return blockColorVanilla(FastMapManager.MANAGER.getFastWorldMapper().getWorld(), worldChunk, pos);
-	}
-
-	private int blockColorPos(World world, BlockPos pos, BlockState blockState) {
-		WorldChunk worldChunk = world.getWorldChunk(pos);
-		return blockColorVanilla(world, worldChunk, pos);
-	}
-
-	private int blockColorVanilla(World world, WorldChunk worldChunk, BlockPos pos) {
-		ChunkPos chunkPos = worldChunk.getPos();
-		int chunkRelX = getChunkRelativeX(pos);
-		int chunkRelZ = getChunkRelativeZ(pos);
-		int shade = 0;
-
-		// FIXME: Redundant if coming here through chunk updates
-		int maxY = getTopBlockY(worldChunk, chunkRelX, chunkRelZ);
-		MapColor mapColor;
-		if (maxY <= world.getBottomY()) {
-			mapColor = MapColor.CLEAR;
-		} else {
-			BlockPos.Mutable thisBlockPos = new BlockPos.Mutable();
-			thisBlockPos.set(pos);
-
-			// Find top-most solid block (without mapcolor "clear")
-			BlockState blockState = worldChunk.getBlockState(thisBlockPos);
-			while (blockState.getMapColor(world, thisBlockPos) == MapColor.CLEAR && maxY > world.getBottomY()) {
-				maxY--;
-				thisBlockPos.set(chunkPos.getStartX() + chunkRelX, maxY, chunkPos.getStartZ() + chunkRelZ);
-				blockState = worldChunk.getBlockState(thisBlockPos);
-			}
-
-			// Is top-most block a fluid?
-			if (maxY > world.getBottomY() && !blockState.getFluidState().isEmpty()) {
-				if (blockState.getFluidState().isIn(FluidTags.WATER)) {
-					// Calculate water depth
-					BlockPos.Mutable fluidBlockPos = new BlockPos.Mutable();
-					fluidBlockPos.set(thisBlockPos);
-					int fluidMaxY = maxY;
-					BlockState fluidBlockState;
-					do {
-						fluidMaxY--;
-						fluidBlockPos.setY(fluidMaxY);
-						fluidBlockState = worldChunk.getBlockState(fluidBlockPos);
-					} while (fluidMaxY > world.getBottomY() && !fluidBlockState.getFluidState().isEmpty());
-
-					// For water, calculate shading according to depth
-					int waterDepth = maxY - fluidMaxY;
-					double shadeArg = waterDepth * 0.1d + (chunkRelX + chunkRelZ & 1) * 0.2d;
-					if (shadeArg < 0.5d) {
-						shade = 2;
-					} else if (shadeArg > 0.9d) {
-						shade = 0;
-					} else {
-						shade = 1;
-					}
-				}
-
-				// Set block state from fluid instead, if applicable
-				if (!blockState.isSideSolidFullSquare(world, thisBlockPos, Direction.UP)) {
-					blockState = blockState.getFluidState().getBlockState();
-				}
-			} else {
-				// For terrain, calculate shading according to height difference
-				// with north (z-1) neighbor
-				// FIXME: this breaks at chunk border!
-				double northMaxY = getHeight(chunkRelX, chunkRelZ - 1);
-				double shadeArg = (maxY - northMaxY) * 4/5.0d
-						+ ((chunkRelX + chunkRelZ & 1) - 0.5d) * 0.4d;
-				if (shadeArg > 0.6d) {
-					shade = 2;
-				} else if (shadeArg < -0.6d) {
-					shade = 0;
-				} else {
-					shade = 1;
-				}
-			}
-			mapColor = blockState.getMapColor(world, thisBlockPos);
-			setBlockState(chunkRelX, chunkRelZ, blockState);
-		}
-
-		if (mapColor == MapColor.CLEAR) {
-			return Colors.BLACK;
-		} else {
-			return ABGRtoARGB(MapColor.COLORS[mapColor.id].getRenderColor(shade));
-		}
 	}
 }
